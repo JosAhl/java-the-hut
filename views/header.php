96349326dae20b91a96b05d24334362b3e0696b0
JosAhl <josefineahlstrand@live.se>
2024-12-31 16:33:40 +0100

    Changed img name

--- views/header.php
+++ views/header.php
@@ -10,17 +10,18 @@
     <link rel="stylesheet" type="text/css" href="/css/features.css">
     <link rel="stylesheet" type="text/css" href="/css/rooms.css">
     <link rel="stylesheet" type="text/css" href="/css/calendar.css">
+    <link rel="stylesheet" type="text/css" href="/css/footer.css">
 </head>
 
 <body>
     <header>
         <h1>Java the Hut</h1>
         <section class="star-rating">
-            <img src="/assets/pngkey.com-deathstar-png-1240061.png" class="stars">
-            <img src="/assets/pngkey.com-deathstar-png-1240061.png" class="stars">
-            <img src="/assets/pngkey.com-deathstar-png-1240061.png" class="stars">
-            <img src="/assets/pngkey.com-deathstar-png-1240061.png" class="stars">
-            <img src="/assets/pngkey.com-deathstar-png-1240061.png" class="stars">
+            <img src="/assets/star.png" class="stars">
+            <img src="/assets/star.png" class="stars">
+            <img src="/assets/star.png" class="stars">
+            <img src="/assets/star.png" class="stars">
+            <img src="/assets/star.png" class="stars">
         </section>
         <nav>
             <a href="/index.php">Hotel</a>
