a12c53391672d78cdffd42529b38d4f43a9955fb
JosAhl <josefineahlstrand@live.se>
2025-01-09 21:52:46 +0100

    Fixed styling

--- features.php
+++ features.php
@@ -7,88 +7,104 @@ require __DIR__ . '/views/header.php';
 <section class="features">
     <section class="feature-info">
 
-        <h3>Bathtub</h3>
         <img src="./assets/baththub2x.png" alt="Bathtub">
-        <p>
-            Indulge in the luxury of a futuristic bathtub, designed to provide maximum relaxation.
-            With ergonomic curves, advanced temperature controls, and optional ambient lighting,
-            it's the perfect way to unwind after a long day. If you ask nicely our staff may also
-            help you scrub your back.
-        </p>
+        <div class="feature-text">
+            <h3>Bathtub</h3>
+            <p>
+                Indulge in the luxury of a futuristic bathtub, designed to provide maximum relaxation.
+                With ergonomic curves, advanced temperature controls, and optional ambient lighting,
+                it's the perfect way to unwind after a long day. If you ask nicely our staff may also
+                help you scrub your back.
+            </p>
+        </div>
 
     </section>
 
     <section class="feature-info">
-        <h3>Pool</h3>
-        <p>
-            Dive into relaxation with the R2D2-inspired pool, a tribute to one of the galaxy's most
-            beloved droids. Featuring glowing blue and white lighting, droid-themed tiles, and unique
-            design elements. You decide where you want to enjoy this feature, you can inflate it in
-            your room or use it as a raft crossing the sea.
-        </p>
+        <div class="feature-text">
+            <h3>Pool</h3>
+            <p>
+                Dive into relaxation with the R2D2-inspired pool, a tribute to one of the galaxy's most
+                beloved droids. Featuring glowing blue and white lighting, droid-themed tiles, and unique
+                design elements. You decide where you want to enjoy this feature, you can inflate it in
+                your room or use it as a raft crossing the sea.
+            </p>
+        </div>
         <img src="./assets/pool2x.png" alt="Pool">
     </section>
 
     <section class="feature-info">
-        <h3>Bicycle</h3>
         <img src="./assets/bike2x.png" alt="Bicycle">
-        <p>
-            Embrace the adventurous spirit of a Mandalorian with this rugged, sci-fi-inspired bicycle.
-            Its durable design and sleek aesthetic make it perfect for exploring diverse terrains in the
-            dusty desert plains of Tatooine. With smooth handling and a futuristic touch it's a
-            suitable transport for both short and long visits.
-        </p>
+        <div class="feature-text">
+            <h3>Bicycle</h3>
+            <p>
+                Embrace the adventurous spirit of a Mandalorian with this rugged, sci-fi-inspired bicycle.
+                Its durable design and sleek aesthetic make it perfect for exploring diverse terrains in the
+                dusty desert plains of Tatooine. With smooth handling and a futuristic touch it's a
+                suitable transport for both short and long visits.
+            </p>
+        </div>
     </section>
 
     <section class="feature-info">
-        <h3>Superiorbar</h3>
-        <p>
-            Awaken your senses at the Java Bar, where rich, aromatic brews meet galactic creativity.
-            From classic espresso shots to exotic blends sourced from planets across the galaxy,
-            this bar offers a unique fusion of flavor and culture. Pair your drink with a
-            selection of interstellar treats and enjoy.
-        </p>
+        <div class="feature-text">
+            <h3>Superiorbar</h3>
+            <p>
+                Awaken your senses at the Java Bar, where rich, aromatic brews meet galactic creativity.
+                From classic espresso shots to exotic blends sourced from planets across the galaxy,
+                this bar offers a unique fusion of flavor and culture. Pair your drink with a
+                selection of interstellar treats and enjoy.
+            </p>
+        </div>
         <img src="./assets/javabar2x.png" alt="Java bar">
     </section>
 
     <section class="feature-info">
-        <h3>Tv</h3>
         <img src="./assets/tv2x.png" alt="TV hologram">
-        <p>
-            Revolutionize your viewing experience with our cutting-edge hologram TV. This immersive
-            display projects entertainment into 3D space, bringing your favorite galactic news, movies,
-            or games to life. You have access to Nitflex and Naboo Prime to enjoy your personal favorites.
-        </p>
+        <div class="feature-text">
+            <h3>Tv</h3>
+            <p>
+                Revolutionize your viewing experience with our cutting-edge hologram TV. This immersive
+                display projects entertainment into 3D space, bringing your favorite galactic news, movies,
+                or games to life. You have access to Nitflex and Naboo Prime to enjoy your personal favorites.
+            </p>
+        </div>
     </section>
 
     <section class="feature-info">
-        <h3>Lightsaber</h3>
-        <p>
-            Unleash the Force with a fully customizable lightsaber, available in a range of hilt designs and
-            blade colors. Perfect for ceremonial use, dueling practice, or simply adding a touch of Star Wars
-            authenticity to your stay. Just make sure not to slice the cleaning droids or break the panoramic
-            window.
-        </p>
+        <div class="feature-text">
+            <h3>Lightsaber</h3>
+            <p>
+                Unleash the Force with a fully customizable lightsaber, available in a range of hilt designs and
+                blade colors. Perfect for ceremonial use, dueling practice, or simply adding a touch of Star Wars
+                authenticity to your stay. Just make sure not to slice the cleaning droids or break the panoramic
+                window.
+            </p>
+        </div>
         <img src="./assets/lightsaber2x.png" alt="Lightsaber">
     </section>
 
     <section class="feature-info">
-        <h3>Car</h3>
         <img src="./assets/podracer2x.png" alt="Car">
-        <p>
-            Channel your inner podracing champion with this high-performance vehicle, built for speed and thrills.
-            Perfect for adrenaline junkies, the podracer combines sleek design with powerful engines, making it ideal
-            for navigating challenging landscapes or making a dramatic entrance. Feel the rush as you zoom through the
-            plains and keep an eye out in the lobby to sign up for the next race.
-        </p>
+        <div class="feature-text">
+            <h3>Car</h3>
+            <p>
+                Channel your inner podracing champion with this high-performance vehicle, built for speed and thrills.
+                Perfect for adrenaline junkies, the podracer combines sleek design with powerful engines, making it ideal
+                for navigating challenging landscapes or making a dramatic entrance. Feel the rush as you zoom through the
+                plains and keep an eye out in the lobby to sign up for the next race.
+            </p>
+        </div>
     </section>
     <section class="feature-info">
-        <h3>RubiksCube</h3>
-        <p>
-            Challenge your mind and test your skills with the Holocron Rubik's Cube, a Star Wars-inspired puzzle
-            crafted to look like an ancient artifact. With glowing edges and a mesmerizing design,
-            this interactive cube combines the fun of a classic brain teaser with the mystique of galactic lore.
-        </p>
+        <div class="feature-text">
+            <h3>RubiksCube</h3>
+            <p>
+                Challenge your mind and test your skills with the Holocron Rubik's Cube, a Star Wars-inspired puzzle
+                crafted to look like an ancient artifact. With glowing edges and a mesmerizing design,
+                this interactive cube combines the fun of a classic brain teaser with the mystique of galactic lore.
+            </p>
+        </div>
         <img src="./assets/holocron2x.png" alt="Rubiks cube">
     </section>
 </section>
